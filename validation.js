6a5297c8c160daecc47219c559687adc50011eb1
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-23 13:05:35 +0200

    add styles and validate phone number

--- validation.js
+++ validation.js
@@ -1,1 +1,13 @@
 // validation script here
+let reg=/^\d{11}$/;
+let tel=document.getElementById("tel");
+console.log(tel);
+tel.addEventListener("keyup",()=>{
+    // console.log(tel.value);
+    console.log(reg);
+    if(reg.test(tel.value)){ 
+        tel.classList.add("validated");
+    }else{
+        tel.classList.remove("validated");
+    }
+});
